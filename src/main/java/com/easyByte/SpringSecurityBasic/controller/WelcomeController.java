2cae19c5db26f28423286ae6d90e83d25a17c778
Adarsh Tripathi <56520511+Adarsh345@users.noreply.github.com>
2024-04-19 20:42:10 +0530

    This is the master commit.

---
+++ src/main/java/com/easyByte/SpringSecurityBasic/controller/WelcomeController.java
@@ -0,0 +1,12 @@
+package com.easyByte.SpringSecurityBasic.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+    @GetMapping("/welcome")
+  public String sayWelcome(){
+    return "Welcome to Spring Boot with out security";
+  }
+}
