8cdbd77ac210b9467624e46cdf6e3d92a2e64a58
Adarsh Tripathi <56520511+Adarsh345@users.noreply.github.com>
2024-05-01 15:11:32 +0530

    Did configuration of authenticated and unauthenticated urls.

--- src/main/java/com/easyByte/config/ProjectSecurityConfig.java
+++ src/main/java/com/easyByte/config/ProjectSecurityConfig.java
@@ -1,15 +1,10 @@
 package com.easyByte.config;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
-
-import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class ProjectSecurityConfig {
