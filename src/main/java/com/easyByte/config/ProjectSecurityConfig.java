a508551d01c5bf9f2c2681dd78a78a5c1b42beaa
Adarsh Tripathi <56520511+Adarsh345@users.noreply.github.com>
2024-05-01 16:21:50 +0530

    Configuration for 1. permit all urls 2. deny all urls 3. Configure urls based on our need.

--- src/main/java/com/easyByte/config/ProjectSecurityConfig.java
+++ src/main/java/com/easyByte/config/ProjectSecurityConfig.java
@@ -11,24 +11,40 @@ public class ProjectSecurityConfig {
     // In spring security first Http request is intercepted by spring security filter
     // so specify type and configurtion of spring security we need configure spring security filter.
 
+    /*
+    * This is the configuration for some of the urls which need authencation and some of the urls
+    * need to tobe authencated.
+    * */
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.
+                        requestMatchers("/myAccount", "/myBalance", "/myLonas", "/myCards").authenticated()
+                        .requestMatchers("/notices", "/contact").permitAll())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
 
-    /*@Bean
- SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-     http.authorizeHttpRequests((requests) -> requests.
-             requestMatchers("/myAccount", "/myBalance", "/myLonas", "/myCards").authenticated()
-             .requestMatchers("/notices", "/contact").permitAll())
-             .formLogin(Customizer.withDefaults())
-             .httpBasic(Customizer.withDefaults());
-     return http.build();
- }
-*/
 
-     @Bean
+    /*
+     * Configuration for deny all request
+     */
+    /* @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests((requests) -> requests.
+                       anyRequest().denyAll());
+        return http.build();
+    } */
+
+    /*
+     * configuration for permit all request
+     * */
+   /* @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests((requests) -> requests.
                        anyRequest().permitAll());
         return http.build();
-    }
+    }*/
 
 
 }
