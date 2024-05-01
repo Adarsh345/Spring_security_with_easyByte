7f8716da6f6f90c6509c51e2e8efa102f4805d11
Adarsh Tripathi <56520511+Adarsh345@users.noreply.github.com>
2024-05-01 14:27:16 +0530

    Add a new esayBank application with some extra controller. Create a bean of SecurityFilterChainConfiguration class in to it so that, I can give custom logic for security.

--- src/main/java/com/easyByte/SpringSecurityBasic/SpringSecurityBasicApplication.java
+++ src/main/java/com/easyByte/SpringSecurityBasic/SpringSecurityBasicApplication.java
@@ -2,6 +2,14 @@ package com.easyByte.SpringSecurityBasic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+/*
+* Filter : Spring security filter extract username and password from http request and
+* converts it in to the Authentication object .
+*
+*
+*
+*
+* */
 
 @SpringBootApplication
 public class SpringSecurityBasicApplication {
